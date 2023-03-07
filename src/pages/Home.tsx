8e8ae601b57c65be94c7c61738f7a492f6f31970
JwRicauter <jwricauter@gmail.com>
2023-03-07 14:41:43 +0100

    adding state card component to the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,26 @@
+/* react functions */
 import { useState, useEffect } from "react";
+
+/* components */
 import { Bar } from "../components/Bar/Bar";
+import { CardFlag } from "../components/Card/Card";
+
+/* services */
 import { getData } from "../services/api";
 
 
 export const Home = () => {
 
-    const [year, setYear] = useState('2019');
-    const [measure, setMeasure] = useState('Property Value');
-    const [growthPeriod, setGrowthPeriod] = useState('1 Year')
-		const [data, setData] = useState([]);
+    const [year, setYear] = useState<string>('2019');
+    const [measure, setMeasure] = useState<string>('Household Income');
+    const [growthPeriod, setGrowthPeriod] = useState<string>('1 Year')
+		const [data, setData] = useState<(any)[]>([]);
 
 
 		useEffect(() => {
-
 			const controller = new AbortController();
     	const signal = controller.signal;
-
+			setData([]);
 			getData({year, measure, growthPeriod, signal}).then(res => {
 				setData(res);
 			})
@@ -27,32 +32,45 @@ export const Home = () => {
 
 
     return (
-        <>
+				<div className='d-flex flex-wrap content-width'>
 					<Bar 
 						setYear={setYear}
 						setMeasure={setMeasure}
 						setGrowthPeriod={setGrowthPeriod}
 					/>
 
-					{
-						data.length > 0 && data.map((state, index) => {
-							let slugState = state[0];
-							let growth = state[1]['growth']
-							let measureThisYear = state[1][parseInt(year)]
+					<div className='flex-column overflow w-75 p-5'>
+						<div className='row'>
+							{
+								data.length > 0 ? data.map((state, index) => {
+									let slugState : string = state[0];
+									let stateName : string = state[0].split('-').join(' ');
+									let growth : number = state[1]['growth']
+									let measureThisYear : number = state[1][parseInt(year)]
+									return (
+										<CardFlag 
+											slugState={slugState}
+											stateName={stateName}
+											growth={growth}
+											measureThisYear={measureThisYear}
+											index={index}
+											key={index}
+											measure={measure}
+										/>
+									)
+									}) :
+									<img
+										src={require('../media/images/spinner.gif')}
+										width="20"
+										height="20"
+										style={{width : '100px', height : 'auto'}}
+										className="d-block mx-auto mt-5"
+										alt="spinner"
+									/>
+								}
+						</div>						
+					</div>
 
-							console.log(slugState)
-							console.log(growth)
-							console.log(measureThisYear)
-							return (
-								<></>
-							)
-
-						})
-					}
-
-
-
-					
-        </>
+        </div>
     )
 }
