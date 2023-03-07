e5a55b17dd10ceaea202324b49cda4adbdb86b9e
JwRicauter <jwricauter@gmail.com>
2023-03-07 12:15:29 +0100

    making the api call

---
+++ src/services/api.ts
@@ -0,0 +1,30 @@
+import { constructURL, orderByGrowth, getStartingYear } from "./helpers"
+
+type Params = {
+	year: string;
+	measure: string;
+	growthPeriod: string;
+	signal: AbortSignal;
+}
+
+
+
+export const getData = async ({year, measure, growthPeriod, signal}: Params)  => {
+	
+	let startingYear = getStartingYear({year, growthPeriod})
+	let uri = await constructURL({year, measure, startingYear});
+
+	let response = await fetch( uri , {
+		method: 'GET',
+		signal: signal
+	}).then(res => res.json())
+	.then(res => {
+		return orderByGrowth(res.data, year, startingYear) 
+	}).catch(
+		err => {return err}
+	)
+
+	return response;
+
+}
+
